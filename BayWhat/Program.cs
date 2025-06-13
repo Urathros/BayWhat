f89140d6a57960926634b84e4a9220869059a708
Urathros <marcus.schaal@web.de>
2025-06-13 21:23:03 +0200

    Add project files.

---
+++ BayWhat/Program.cs
@@ -0,0 +1,13 @@
+namespace BayWhat
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+          
+        }
+    }
+}
